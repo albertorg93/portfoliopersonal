43c55e7600095cf9461a357bf1ac686d2dff2412
albertorg93 <futberto1993@gmail.com>
2024-09-11 17:20:45 +0200

    modify margin picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                 transition={{duration: 0.5}}
                 className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex items-center justify-center">
-                    <img className="rounded-2xl mt-2 " src={other} alt="about"  width={300} height={300}  />
+                    <img className="rounded-2xl" src={other} alt="about"  width={300} height={300}  />
                 </div>
                 </motion.div>
                 <motion.div
